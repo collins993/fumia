85677395e8164b979ec6221e58d7a901a8cdb52a
Collins Nze <74765532+collins993@users.noreply.github.com>
2023-07-12 10:58:50 +0100

    first commit

---
+++ fumia/pages/_app.js
@@ -0,0 +1,24 @@
+import '@/styles/globals.css';
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Layout, layout } from '../components';
+import { StateContext } from '../context/StateContext';
+
+
+export default function App({ Component, pageProps }) {
+  return (
+
+
+
+    <StateContext>
+
+      <Layout>
+
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+
+
+  )
+}
